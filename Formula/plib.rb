7c42a56d7d44f60caf688cab7d766c571f68d9d3
Chris Chow <chris.kr.chow@gmail.com>
2022-01-01 14:36:23 -0800

    fix audit nits

--- Formula/plib.rb
+++ Formula/plib.rb
@@ -1,12 +1,11 @@
 class Plib < Formula
   desc "Steve's portable game library"
-  homepage "http://plib.sourceforge.net/index.html"
+  homepage "https://plib.sourceforge.io/index.html"
   url "http://plib.sourceforge.net/dist/plib-1.8.5.tar.gz"
   sha256 "485b22bf6fdc0da067e34ead5e26f002b76326f6371e2ae006415dea6a380a32"
-  license "LGPL"
+  license "LGPL-2.0-only"
 
   patch :p1, :DATA
-
 
   def install
     system "./configure", "--disable-debug",
@@ -20,20 +19,20 @@ class Plib < Formula
 
   test do
     (testpath/"test.cpp").write <<~EOS
-    #include <plib/sg.h>
-    #include <plib/fnt.h>
-    #include <plib/js.h>
-    #include <plib/net.h>
-    #include <plib/psl.h>
-    #include <plib/puAux.h>
-    #include <plib/ul.h>
-    int main() {
-      sgVec3 Euler;
-      sgSetVec3( Euler, 1, 1, 1 ) ;
-      return 0;
-    }
+      #include <plib/sg.h>
+      #include <plib/fnt.h>
+      #include <plib/js.h>
+      #include <plib/net.h>
+      #include <plib/psl.h>
+      #include <plib/puAux.h>
+      #include <plib/ul.h>
+      int main() {
+        sgVec3 Euler;
+        sgSetVec3( Euler, 1, 1, 1 ) ;
+        return 0;
+      }
     EOS
-    system ENV.cc, "test.cpp", "-L#{lib}", 
+    system ENV.cc, "test.cpp", "-L#{lib}",
     "-lplibsg", "-lplibfnt", "-lplibjs", "-lplibnet", "-lplibpsl", "-lplibpuAux",
     "-lplibul",
     "-o", "test"
@@ -41,7 +40,7 @@ class Plib < Formula
   end
 end
 
-# note: this patch comes from https://trac.macports.org/browser/trunk/dports/devel/plib/files
+# NOTE: this patch comes from https://trac.macports.org/browser/trunk/dports/devel/plib/files
 # via https://gist.github.com/mkroehnert/effb45389e14fc28c286
 __END__
 diff --git a/src/pui/puGLUT.h b/src/pui/puGLUT.h
